9b798b4bc008c2e30b68a667ff58022573186c95
shinewen189 <61495217@qq.com>
2021-09-07 17:42:56 +0800

    自动部署代码测试

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,21 +1,42 @@
 <template>
-  <div class="box">
-    <span>这是一个测试程序</span>
+  <div>
+    <div class="box2"></div>
+    <div class="box">
+      <span>这是一个测试程序！！ + {{ test }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
-  mounted() {}
+  data() {
+    return {
+      test: 0
+    }
+  },
+  mounted() {
+    this.test = this.getData(2, 3)
+  },
+  methods: {
+    getData(a, b) {
+      return a + b
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.box2 {
+  width: 100px;
+  height: 100px;
+  background-color: aqua;
+}
 .box {
   width: 500px;
   height: 500px;
   background-color: #000;
+
   span {
     width: 100px;
     height: 100px;
